0526567e6511d94b4bbfbb3a82e8e25a6ad28040
oprokopieva382 <oprokopieva382@gmail.com>
2022-08-17 17:38:22 -0500

    added img API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsRespons(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -17,6 +23,12 @@ export default function Dictionary() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001bad56e1f550b472d8ffe41a56d1835d1";
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsRespons);
   }
 
   function handleKeywordChange(event) {
@@ -39,6 +51,7 @@ export default function Dictionary() {
         </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
