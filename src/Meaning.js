3fb18af10e28f8cae50bd82e3f3d1dda062ea9e3
oprokopieva382 <oprokopieva382@gmail.com>
2022-08-17 09:36:00 -0500

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
+            <p>
+              <strong>Definition: </strong>
+              {definition.definition}
+            </p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
